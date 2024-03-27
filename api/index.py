e7cc4bc3b7927364bc89b8bae30ed77d3c9d6649
Kauan Sena <lkrs@protonmail.com>
2024-03-27 20:21:21 -0300

    prod deploy

--- api/index.py
+++ api/index.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from main import Websites
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return {
+        "status": 200,
+        "endpoints": {
+            "/news": ["/uol", "/g1", "/investing", "/infomoney"]
+        },
+        "info": "Econnews, a API aberta de notícias sobre finanças e economia"
+    }
+
 
 @app.route('/status')
 def about():
     return {"status": 200, "message": "Flask server running"}
+
+
+@app.route('/uol')
+def uol_endoint():
+    website = Websites()
+    news = website.uol()
+    return {"status": 200, "uol": news}
+
+app.run(debug=True)
---
+++ api/main.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from requests import get
+import json
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
+
+class Websites:
+    def __init__(self):
+        pass
+
+
+    def uol(self):
+        url = 'https://economia.uol.com.br/ultimas/'
+        req = get(url, headers=headers)
+        html = BeautifulSoup(req.text, 'html.parser')
+        elements = html.find_all('div', class_='thumbnails-wrapper')
+
+        titles = []
+        links = []
+        response = {}
+
+        for element in elements:
+            title = element.find('h3').get_text()
+            link = element.find('a')['href']
+
+            response.update({title: link})
+            titles.append(title)
+            links.append(link)
+        
+        return response
+
+
+    def g1(self):
+        pass
+
+
+    def infomoney(self):
+        pass
